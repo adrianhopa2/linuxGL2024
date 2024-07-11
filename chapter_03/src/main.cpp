782a0e9f5c174973faa9419300fda177de8802be
adrianhopa2 <thegamr3@gmail.com>
2024-07-11 10:06:40 +0200

    chapter_03

--- chapter_03/src/main.cpp
+++ chapter_03/src/main.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
-#include <memory>
-
-
-class SmartPointer
-{
-private:
-    int *ptr;
-
-public:
-    SmartPointer(int *pointer = nullptr) : ptr{pointer}
-    {
-        std::cout << "SmartPointer constructor" << std::endl;
-    }
-
-    ~SmartPointer()
-    {
-        std::cout << "SmartPointer destructor" << std::endl;
-        delete ptr;
-    }
-
-    int& operator*() { return *ptr; }
- 
-    int* operator->() { return ptr; }
-};
 
 int main()
 {
-    SmartPointer sptr(new int(7));
-
-    std::cout << *sptr << std::endl;
+    std::cout << "Hello World!" << std::endl;
 
     return 0;
 }
