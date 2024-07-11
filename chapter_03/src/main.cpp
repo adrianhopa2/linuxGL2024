0d21a7fe615b5f86b59074c952cb018b90746ec0
adrianhopa2 <thegamr3@gmail.com>
2024-07-11 14:29:58 +0200

    chapter_03

--- chapter_03/src/main.cpp
+++ chapter_03/src/main.cpp
@@ -25,47 +25,6 @@ public:
 
 };
 
-class Test
-{
-private:
-    int x;
-    int y;
-
-public:
-    Test(int X = 0, int Y = 0) : x{X}, y{Y} {}
-
-    int getX()
-    {
-        return x;
-    }
-
-    int getY()
-    {
-        return y;
-    }
-};
-
-class customTestClassAllocator
-{
-private:
-    Test t;
-
-public:
-    customTestClassAllocator(Test T = Test()) : t{T}
-    {
-        std::cout << "Custom Test object Allocated" << std::endl;
-    }
-
-    ~customTestClassAllocator()
-    {
-        std::cout << "Custom Test object Destructor" << std::endl;
-    }
-
-    Test getTest()
-    {
-        return t;
-    }
-};
 
 
 
@@ -87,12 +46,6 @@ int main()
     std::cout << ptr2.use_count() << std::endl;
     std::cout << ptr2->getInt() << std::endl << std::endl;
 
-
-    //custom class allocator
-    shared_ptr<customTestClassAllocator> ptr3 = std::make_shared<customTestClassAllocator>(Test(2, 4));
-
-    std::cout << ptr3.use_count() << std::endl;
-    std::cout << ptr3->getTest().getX() << " " << ptr3->getTest().getY() << std::endl << std::endl;
 
   
 
